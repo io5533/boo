9de1726ab15ddedc3f710d8fd4a1b62093c74a32
io5533 <io5533@users.noreply.github.com>
2026-07-10 20:41:29 +0900

    doc: english comment

--- arm64/boo.s
+++ arm64/boo.s
@@ -2,20 +2,20 @@
 
 .section .text
 _start:
-    mov     x8, #175 // sys_fork (또는 sys_clone)
+    mov     x8, #175 // sys_geteuid
     svc     #0
 
-    cbnz    x0, 1f  // 부모 프로세스라면 종료 라벨(1:)로 점프
+    cbnz    x0, 1f   // Exit if EUID is not 0 (non-root)
 
-    mov     x0, #1  // stdout
+    mov     x0, #1   // stdout
     adr     x1, msg
-    mov     x2, #10
-    mov     x8, #64 // sys_write
+    mov     x2, #10  // Message length
+    mov     x8, #64  // sys_write
     svc     #0
 
 1:
-    mov     x8, #93 // sys_exit
-    mov     x0, #0       
+    mov     x8, #93  // sys_exit
+    mov     x0, #0   // Exit status
     svc     #0
 
 msg:
